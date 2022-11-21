1a94d39472b657c0a5451389f80c2f7b91b418da
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-11-21 17:13:11 +0300

    Update

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { createBuiltMeshHTTPHandler } from '../.mesh'
+import { createBuiltMeshHTTPHandler } from './.mesh'
 
 // Pass Mesh's HTTP handler as an event listener
 self.addEventListener('fetch', createBuiltMeshHTTPHandler())
